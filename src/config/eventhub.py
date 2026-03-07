f36f74f0843eed4455b1a94191e6a74861e994c8
dssatpute <dssatpute>
2026-03-07 20:18:18 +0530

    Set failOnDataLoss to 'false' in PLAYBACK_EVENTS_KAFKA_OPTIONS

--- src/config/eventhub.py
+++ src/config/eventhub.py
@@ -29,6 +29,6 @@ PLAYBACK_EVENTS_KAFKA_OPTIONS = {
   "kafka.request.timeout.ms" :10000,
   "kafka.session.timeout.ms" : 10000,
   "maxOffsetsPerTrigger"     : 10000,
-  "failOnDataLoss"           : 'true',
+  "failOnDataLoss"           : 'false',
   "startingOffsets"          : "latest"
 }
